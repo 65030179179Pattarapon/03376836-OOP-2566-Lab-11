49f9d91a2aa10db32d40cbe3b6062d8a98edbd04
PATTARAPON PONKUL <65030179@kmitl.ac.th>
2024-03-30 13:22:29 +0700

    commit Ex03

--- 65030179_Lab11/Lab11_Ex03/Program.cs
+++ 65030179_Lab11/Lab11_Ex03/Program.cs
@@ -1,2 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var animalA = new Dog();
+var animalB = new Fish();
+var animalC = new Bird();
+
+animalA.Move();
+animalB.Move();
+animalC.Move();
+
+class Animal
+{
+   public virtual void Move()
+   {
+    System.Console.WriteLine("Move successfully.");
+   } 
+}
+
+class Dog:Animal
+{
+    public override void Move()
+    {
+        System.Console.WriteLine($"{this.GetType()}: Running on the ground.");
+        base.Move();
+    }
+}
+class Fish: Animal
+{
+    public override void Move()
+    {
+        System.Console.WriteLine($"{this.GetType()}: Swimming in the water.");
+        base.Move();
+    }
+}
+
+class Bird: Animal
+{
+    public override void Move()
+    {
+        System.Console.WriteLine($"{this.GetType()}: Flying in the air.");
+        base.Move();
+    }
+}
