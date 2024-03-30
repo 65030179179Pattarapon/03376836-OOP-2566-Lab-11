41e5a547e2cb62dc69f51810690e11d3f9f35200
PATTARAPON PONKUL <65030179@kmitl.ac.th>
2024-03-30 13:27:07 +0700

    commit Ex04

--- 65030179_Lab11/Lab11_Ex04/Program.cs
+++ 65030179_Lab11/Lab11_Ex04/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var b = new Base();
+var d1 = new Derived_1();
+var d2 = new Derived_2();
+
+b.A();
+d1.A();
+d2.A();
+
+class Base
+{
+    public virtual void A()
+    {
+        System.Console.WriteLine("Base.A()");
+    }
+}
+class Derived_1 : Base
+{
+    public override void A()
+    {
+        System.Console.WriteLine("Derived_1.A()");
+    }
+}
+class Derived_2 : Derived_1
+{
+    public override void A()
+    {
+        System.Console.WriteLine("Derived_2.A()");
+    }
+}
