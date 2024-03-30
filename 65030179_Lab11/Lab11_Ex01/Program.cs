9f405b8d4a344698996e93bccffdea8dfc84b73d
PATTARAPON PONKUL <65030179@kmitl.ac.th>
2024-03-30 12:26:36 +0700

    commit Ex01

--- 65030179_Lab11/Lab11_Ex01/Program.cs
+++ 65030179_Lab11/Lab11_Ex01/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿System.Console.WriteLine(Calculator.AddValues(2, 3));
+System.Console.WriteLine(Calculator.AddValues(2, 3, 4));
+System.Console.WriteLine(Calculator.AddValues(2.5f, 3.7f));
+System.Console.WriteLine(Calculator.AddValues(2L, 3L));
+
+public static class Calculator
+{
+    public static long AddValues(int a, int b)
+    {
+        System.Console.WriteLine("Calculated by method: public static long AddValues(int a, int b)");
+        return a + b;
+    }
+    public static long AddValues(int c, int d, int e)
+    {
+        System.Console.WriteLine("Calculated by method: public static long AddValues(int c, int d, int e)");
+        return c + d + e;
+    }
+    public static long AddValues(float f, float g)
+    {
+        System.Console.WriteLine("Calculated by method: public static long AddValues(float f, float g)");
+        return (long) (f + g);
+    }
+   public static long AddValues(long h, long i)
+    {
+        System.Console.WriteLine("Calculated by method: public static long AddValues(long h, long i)");
+        return (long) (h + i);
+    }
+}
